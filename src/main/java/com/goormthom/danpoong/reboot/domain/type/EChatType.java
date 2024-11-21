49ef76f412394ab09c1b53a992b78f1247d0cfd6
park20011029 <rokmck156@gmail.com>
2024-11-21 11:33:37 +0900

    Fix: 채팅 엔티티 변경

--- src/main/java/com/goormthom/danpoong/reboot/domain/type/EChatType.java
+++ src/main/java/com/goormthom/danpoong/reboot/domain/type/EChatType.java
@@ -3,25 +3,27 @@ package com.goormthom.danpoong.reboot.domain.type;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@Getter
+
 @RequiredArgsConstructor
 public enum EChatType {
-    RISE("RISE"),
-    FOLD("FOLD"),
-    WASH("WASH"),
+    RISE("RISE","기상하기"), // 기상
+    FOLD("FOLD","이불정리하기"), // 이불 정리
+    WASH("WASH", "씻기"), // 씻기
 
-    MORNING("MORNING"),
-    LUNCH("LUNCH"),
-    DINNER("DINNER"),
+    MORNING("MORNING", "아침 먹기"), // 아침
+    LUNCH("LUNCH","점심 먹기"), // 점심
+    DINNER("DINNER","저녁 먹기"), // 저녁
 
-    WALK("WALK"),
-    MARKET("MARKET"),
-    PICTURE("PICTURE"),
+    WALK("WALK","산책가기"), // 걷기
+    MARKET("MARKET","장보기"), // 장보기
+    PICTURE("PICTURE", "외부사진찍기"), // 출사
 
-    LEAVE("LEAVE"),
+    LEAVE("LEAVE","퇴근하기"), // 퇴근
     ;
 
     private final String chatType;
+    @Getter
+    private final String description;
 
     public static EChatType fromName(String chatType) {
         return EChatType.valueOf(chatType.toUpperCase());
@@ -31,4 +33,9 @@ public enum EChatType {
     public String toString() {
         return chatType;
     }
+
+    public String description() {
+        return description;
+    }
+
 }
