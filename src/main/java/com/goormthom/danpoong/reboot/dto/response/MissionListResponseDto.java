83eed4ae0d68aa2d90f1f5f41f061522ea484d14
zzangjyj0818 <jyj010818@naver.com>
2024-11-22 17:28:24 +0900

    refactor: MissionListResponseDto 반환값 추가

--- src/main/java/com/goormthom/danpoong/reboot/dto/response/MissionListResponseDto.java
+++ src/main/java/com/goormthom/danpoong/reboot/dto/response/MissionListResponseDto.java
@@ -2,8 +2,13 @@ package com.goormthom.danpoong.reboot.dto.response;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
+
 @Builder
 public record MissionListResponseDto(
-        String mission
+        String mission,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        boolean isDone
 ) {
 }
