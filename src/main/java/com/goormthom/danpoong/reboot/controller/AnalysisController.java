f895a66c8a2d79464dca280a15eeee0eb40c9840
zzangjyj0818 <jyj010818@naver.com>
2024-11-22 23:44:14 +0900

    docs: 일자별 업무들 상세보기 Docs 작성

--- src/main/java/com/goormthom/danpoong/reboot/controller/AnalysisController.java
+++ src/main/java/com/goormthom/danpoong/reboot/controller/AnalysisController.java
@@ -4,15 +4,12 @@ import com.goormthom.danpoong.reboot.annotation.UserId;
 import com.goormthom.danpoong.reboot.controller.docs.AnalysisDocs;
 import com.goormthom.danpoong.reboot.dto.common.ResponseDto;
 import com.goormthom.danpoong.reboot.dto.response.AllAnalysisResponseDto;
-import com.goormthom.danpoong.reboot.usecase.analysis.AllAnalysisUseCase;
-import com.goormthom.danpoong.reboot.usecase.analysis.JournalUseCase;
-import com.goormthom.danpoong.reboot.usecase.analysis.MissionListUseCase;
-import com.goormthom.danpoong.reboot.usecase.analysis.RemainPeriodUseCase;
+import com.goormthom.danpoong.reboot.usecase.analysis.*;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @RestController
@@ -23,6 +20,8 @@ public class AnalysisController implements AnalysisDocs {
     private final RemainPeriodUseCase remainPeriodUseCase;
     private final JournalUseCase journalUseCase;
     private final MissionListUseCase missionListUseCase;
+    private final CalendarUseCase calendarUseCase;
+    private final CalendarDetailUseCase calendarDetailUseCase;
     private final AllAnalysisUseCase allAnalysisUseCase;
 
     @GetMapping("/remain-period")
@@ -40,6 +39,15 @@ public class AnalysisController implements AnalysisDocs {
         return ResponseDto.ok(missionListUseCase.execute(userId));
     }
 
+    @GetMapping("/calendar")
+    public ResponseDto<?> getCalendar(@Parameter(hidden = true) @UserId UUID userId, @RequestParam(name = "groupType") Long groupType) {
+        return ResponseDto.ok(calendarUseCase.execute(userId, groupType));
+    }
+
+    @GetMapping("/calendar-detail")
+    public ResponseDto<?> getCalendarDetail(@UserId UUID userId, @RequestParam(name = "date") LocalDate date) {
+        return ResponseDto.ok(calendarDetailUseCase.execute(userId, date));
+    }
     @GetMapping("/all")
     public ResponseDto<?> getAllAnalysis(@UserId UUID userId) {
         return ResponseDto.ok(allAnalysisUseCase.execute(userId));
